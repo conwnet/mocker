92f227aab249a9543d9bb9408da9c69c34460921
netcon <netcon@live.com>
2018-07-20 14:55:06 +0800

    init frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>Good Boy</div>
+);
+
+ReactDOM.render(
+    <App />,
+    document.body.appendChild(document.createElement('div'))
+);
